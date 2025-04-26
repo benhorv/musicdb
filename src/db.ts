13b35bedb19dea96c5c3639a44398a072a060a0c
Benedek Horváth <benedekhorvath@edu.bme.hu>
2025-04-26 12:23:02 +0200

    change to promise-based approach

--- src/db.ts
+++ src/db.ts
@@ -1,12 +1,9 @@
 import { Database } from 'sqlite3';
+import { open } from 'sqlite';
 
 export async function connectToDb() {
-    const db = new Database("../database/chinook.db", (err) => {
-        if (err) {
-            console.error(err.message);
-        } else {
-            console.log("Connected to database");
-        }
+    return open({
+        filename: '../database/chinook.db',
+        driver: Database,
     });
-    return db;
 }
