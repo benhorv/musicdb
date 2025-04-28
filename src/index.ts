5fd99d8e209e62f8d138bada1d549cb16f42a71c
Benedek Horváth <benedekhorvath@edu.bme.hu>
2025-04-28 19:55:49 +0200

    add db file check

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,17 @@ import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 import { typeDefs } from './schema.js';
 import { resolvers } from './resolvers.js';
+import fs from 'fs';
 
 const server = new ApolloServer({ typeDefs, resolvers });
 try {
     const { url } = await startStandaloneServer(server, {
         listen: { port: 4000, path: '/gql' },
-      });
-    
+    });
+
+    if (!fs.existsSync('./database/chinook.db')) {
+        throw new Error('DB file not found at ./database/chinook.db');
+    }
     console.log(`Server ready at ${url}`);
 } catch (err) {
     console.error('Error starting server: ', err);
